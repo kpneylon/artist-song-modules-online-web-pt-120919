db43556f90ce4562934520c62006e742cb20911d
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-25 03:31:29 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,8 @@
 module Findable
   
-  module ClassMethods
+  def find_by_name(name)
+    detect{|a| a.name == name}
+  end
     
-    def find_by_name(name)
-      detect{|a| a.name == name}
-    end
-  end  
   
 end  
